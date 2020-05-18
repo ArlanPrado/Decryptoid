b8c1233556dabb10e631e4649495afc9cf8ddf3b
ArlanPrado <arlanjprado@gmail.com>
2020-05-17 18:50:41 -0700

    Added DoubleTransposition functionality
    Encryption works, minor bug for 2 whitespaces.
    
    Decryption does not work.

--- doubletransposition.php
+++ doubletransposition.php
@@ -33,6 +33,8 @@ $cipherDubTranspo = <<<HTML
         HTML;
 echo $cipherDubTranspo;
 
+$alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
 if(isset($_POST["btSignOut"])){
     signOut();
@@ -42,7 +44,16 @@ if(isset($_POST["btHome"])){
 }
 if(isset($_POST["btText"])){
     if(isset($_POST["key1"]) && isset($_POST["key2"]) && isset($_POST["textIn"])){
-        
+        $k1 = strtoupper(sanitizeMySQL($conn, $_POST["key1"]));
+        $k2 = strtoupper(sanitizeMySQL($conn, $_POST["key2"]));
+        $t = sanitizeMySQL($conn, $_POST["textIn"]);
+        echo "<b>Key 1</b>: " . $k1 . "<b> Key 2</b>: " . $k2;
+        echo "<br>";
+        echo "<b>Original Text</b>: [" . $t . "]";
+        echo "<br>";
+        echo "<b>Encyrption</b>: [" . dtEncrypt($t, $k1, $k2, $alphabet) . "]";
+        echo "<br>";
+        echo "<b>Decryption</b>: [" . dtDecrypt($t, $k1, $k2, $alphabet) . "]";
     }else{
         echo "No Keys or Text Present";
     }
@@ -59,4 +70,103 @@ if(isset($_POST["btFile"])){
         echo "No Keys or File Present";
     }
 }
+
+
+function dTEncrypt($text, $key1, $key2, $alphabet){
+    $text1 = tEncrypt($text, $key1, $alphabet);
+    $text2 = tEncrypt($text1, $key2, $alphabet);
+    return $text2;
+}
+function dTDecrypt($text, $key1, $key2, $alphabet){
+    $text1 = tDecrypt($text, $key2, $alphabet);
+    $text2 = tDecrypt($text1, $key1, $alphabet);
+    return $text2;
+}
+function tEncrypt($text, $key1, $alphabet){
+    $key1Arr = array();
+    $keyInd = 1;
+    for($j = 0; $j < strlen($alphabet); $j++){      //get the number for each letter in the key1
+        for($i = 0; $i < strlen($key1); $i++){
+            if($alphabet[$j] == $key1[$i]){
+                $key1Arr[$keyInd] = $alphabet[$j];
+                $keyInd++;
+            }
+        }
+    }
+    //echo var_dump($key1Arr);
+   // echo "<br>";
+    $key1wTextArr = array();
+    //strlen($text)
+    for($i = 0; $i < strlen($text); $i++){      //arrange the text letters in columns
+        if($keyInd > strlen($key1)){
+            $keyInd = 1;
+        }
+        if(!isset($key1wTextArr[$keyInd])){
+            $key1wTextArr[$keyInd] = $text[$i];
+        }
+        else{
+            $key1wTextArr[$keyInd] = $key1wTextArr[$keyInd] . $text[$i];
+        }
+        $keyInd++;
+    }
+    $text1 = "";
+    $keyChar2 = NULL;
+    for($i = 1; $i < strlen($key1)+1; $i++){        //order the columns based on alphabetical priority
+        $keyChar = $key1Arr[$i];
+        $strPos = stripos($key1, $keyChar) + 1;
+        if($keyChar2 == $keyChar){              //if the last 
+            $strPos = stripos($key1, $keyChar, $strPos) + 1;
+        }
+        $text1 = $text1 . $key1wTextArr[$strPos];
+        $keyChar2 = $keyChar;        
+    }
+    return $text1;
+}
+function tDecrypt($text, $key1, $alphabet){
+    $key1TwoDim = 
+    
+    /*
+    $key1Arr = array();
+    $keyInd = 1;
+    for($j = 0; $j < strlen($alphabet); $j++){      //get the number for each letter in the key1
+        for($i = 0; $i < strlen($key1); $i++){
+            if($alphabet[$j] == $key1[$i]){
+                $key1Arr[$keyInd] = $alphabet[$j];
+                $keyInd++;
+            }
+        }
+    }
+
+    echo "<br>";
+    echo var_dump($key1Arr);
+    $textNum = strlen($text);
+    $key1Num = strlen($key1);
+    $rows = (int)($textNum / $key1Num);
+    if($textNum % $key1Num > 0){
+        $rows = $rows + 1;
+    }
+    $placeholderNum = ($rows * $key1Num) - $textNum;
+    //echo $rows;
+    $textArr = array();
+    $j = 0;
+    for($i = 0; $i < strlen($key1); $i++){          //organize the substrings into columns
+        $textArr[$i+1] = substr($text, $j, $rows);
+        $j += $rows;
+        echo $textArr[$i+1];
+    }
+    echo "<br>";
+    echo var_dump($textArr);
+    //arrange the columns into the key's order
+    $text1 = array();
+    $keyChar2 = NULL;
+    //key1 is TESTING
+    for($i = 1; $i < strlen($key1)+1; $i++){        //order the columns based on alphabetical priority
+        //$priori = array_search($key1Arr[$i], $key1); //get T's priority
+       // $substring = $textArr[$priori];  //get the index at the same priority as T
+        //$text1[$i] = $substring;//place the column into the array[$i]
+        
+    }
+    */
+}
+
 ?>
